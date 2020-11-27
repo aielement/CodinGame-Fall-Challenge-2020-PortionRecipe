065481c093e1c79dc3fe8e12a99968def052f723
aielement <a.i.0326.element@gmail.com>
2020-11-27 19:26:17 +0900

    初期コミット

---
+++ Assets/DrawPortionRecipe.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+
+public class DrawPortionRecipe : MonoBehaviour
+{
+    TextAsset portionCsvFile;
+    List<PortionRecipe> portionRecipeList = new List<PortionRecipe>();
+
+
+    [SerializeField]
+    GameObject _portionRecipeContentPrefab;
+
+    [SerializeField]
+    Canvas canvas;
+    // Start is called before the first frame update
+    void Start()
+    {
+        LoadPortionRecipeCSV();
+        DrawPortionRecipes();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    void LoadPortionRecipeCSV() {
+        portionCsvFile = Resources.Load("portionRecipe") as TextAsset; // Resouces下のCSV読み込み
+        StringReader reader = new StringReader(portionCsvFile.text);
+
+        // , で分割しつつ一行ずつ読み込み
+        // リストに追加していく
+        while (reader.Peek() != -1) // reader.Peaekが-1になるまで
+        {
+            string line = reader.ReadLine(); // 一行ずつ読み込み
+            string[] data = line.Split(',');
+            PortionRecipe portionRecipe = new PortionRecipe();
+            portionRecipe.tier0 = int.Parse(data[0]);
+            portionRecipe.tier1 = int.Parse(data[1]);
+            portionRecipe.tier2 = int.Parse(data[2]);
+            portionRecipe.tier3 = int.Parse(data[3]);
+            portionRecipe.price = int.Parse(data[4]);
+
+            portionRecipeList.Add(portionRecipe); // , 区切りでリストに追加
+        }
+
+    }
+
+    void DrawPortionRecipes() {
+        for (int w = 0; w < 6; w++) {
+            for(int h = 0;h < 6; h++) {
+                GameObject portionRecipeContentObject = Instantiate(_portionRecipeContentPrefab,canvas.transform);
+                RectTransform rectTransform = portionRecipeContentObject.GetComponent<RectTransform>();
+                rectTransform.localPosition = new Vector3(330 * w - 930, 490 - 170 * h, 0) + new Vector3(50, 75, 0);
+                PortionRecipeContent portionRecipeContent = portionRecipeContentObject.GetComponent<PortionRecipeContent>();
+                portionRecipeContent.Set(portionRecipeList[w + 6 * h]);
+            }
+        }
+    }
+
+}
+
+public class PortionRecipe {
+    public int tier0;
+    public int tier1;
+    public int tier2;
+    public int tier3;
+    public int price;
+}
+
---
+++ Assets/PortionRecipeContent.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class PortionRecipeContent : MonoBehaviour
+{
+    const int TIER_MAX_COUNT = 6;
+    const int TIER_TYPE_COUNT = 4;
+
+    [SerializeField]
+    Sprite[] _sprites_tier = new Sprite[TIER_TYPE_COUNT];
+
+    [SerializeField]
+    Image[] _imgs_tier = new Image[TIER_MAX_COUNT];
+
+    [SerializeField]
+    Text _txt_price;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Set(PortionRecipe portionRecipe) {
+        _txt_price.text = portionRecipe.price.ToString();
+
+
+        foreach (Image img_tier in _imgs_tier) {
+            img_tier.gameObject.SetActive(false);
+        }
+
+        int baseIndex = 0;
+
+        for (int i = baseIndex; i < baseIndex + portionRecipe.tier0; i++) {
+            _imgs_tier[i].gameObject.SetActive(true);
+            _imgs_tier[i].sprite = _sprites_tier[0];
+        }
+
+        baseIndex += portionRecipe.tier0;
+
+        for (int i = baseIndex; i < baseIndex + portionRecipe.tier1; i++) {
+            _imgs_tier[i].gameObject.SetActive(true);
+            _imgs_tier[i].sprite = _sprites_tier[1];
+        }
+
+        baseIndex += portionRecipe.tier1;
+
+        for (int i = baseIndex; i < baseIndex + portionRecipe.tier2; i++) {
+            _imgs_tier[i].gameObject.SetActive(true);
+            _imgs_tier[i].sprite = _sprites_tier[2];
+        }
+
+        baseIndex += portionRecipe.tier2;
+
+        for (int i = baseIndex; i < baseIndex + portionRecipe.tier3; i++) {
+            _imgs_tier[i].gameObject.SetActive(true);
+            _imgs_tier[i].sprite = _sprites_tier[3];
+        }
+
+        baseIndex += portionRecipe.tier3;
+    }
+}
